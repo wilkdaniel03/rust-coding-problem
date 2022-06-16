829b2d0b02bb269f02c3ca64cf28b4ba86b23980
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 22:36:41 +0200

    [Two number sum] Solution using pointers.

--- two-number-sum/src/main.rs
+++ two-number-sum/src/main.rs
@@ -38,10 +38,33 @@ impl TwoNumberSum {
         }
         result
     }
+
+    fn find_using_pointers(vector: &mut Vec<i8>, target_sum: i8) -> SumResult {
+        vector.sort();
+        let mut result: SumResult = SumResult::NotFound([]);
+        let mut smallest_idx = 0;
+        let mut largest_idx = vector.len() - 1;
+        while smallest_idx < largest_idx && largest_idx < vector.len() {
+            let possible_result = vector[smallest_idx] + vector[largest_idx];
+            if possible_result < target_sum {
+                smallest_idx += 1;
+            } else if possible_result > target_sum {
+                largest_idx -= 1;
+            } else {
+                result = SumResult::Found([
+                    vector[smallest_idx],
+                    vector[largest_idx]
+                ]);
+                break;
+            }
+        }
+        result
+    }
 }
 
 fn main() {
-    let v: Vec<i8> = vec!(3,5,-4,8,11,1,-1,6);
+    let mut v: Vec<i8> = vec!(3,5,-4,8,11,1,-1,6);
     // println!("{:?}", TwoNumberSum::find_using_nested_loop(v, 10));
-    println!("{:?}", TwoNumberSum::find_using_hash_map(v, 10));
+    // println!("{:?}", TwoNumberSum::find_using_hash_map(v, 10));
+    println!("{:?}", TwoNumberSum::find_using_pointers(&mut v, 10));
 }
