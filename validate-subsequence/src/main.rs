d65709c0693871c9599565e9dace78ad6fa5a1af
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 22:45:27 +0200

    [Validate subsequence] Solution using while loop.

--- validate-subsequence/src/main.rs
+++ validate-subsequence/src/main.rs
@@ -1,3 +1,21 @@
+struct ValidateSubsequence;
+
+impl ValidateSubsequence {
+    fn check_using_while(vector: Vec<i8>, sequence: Vec<i8>) -> bool {
+        let mut vector_idx = 0;
+        let mut sequence_idx = 0;
+        while vector_idx < vector.len() && sequence_idx < sequence.len() {
+            if vector[vector_idx] == sequence[sequence_idx] {
+                sequence_idx += 1;
+            }
+            vector_idx += 1;
+        }
+        sequence_idx == sequence.len()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<i8> = vec!(5,1,22,25,6,-1,8,10);
+    let s: Vec<i8> = vec!(1,6,-1,10);
+    println!("{:?}", ValidateSubsequence::check_using_while(v, s));
 }
