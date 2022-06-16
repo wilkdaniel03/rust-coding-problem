2a19f863cf051619dcb87c2572c4465c9206cdfb
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-16 23:12:01 +0200

    [Sorted squared array] Solution using additional sorting algorithm.

--- sorted-squared-array/src/main.rs
+++ sorted-squared-array/src/main.rs
@@ -1,3 +1,18 @@
+struct SortedSquaredArray;
+
+#[allow(dead_code)]
+impl SortedSquaredArray {
+    fn sort_using_algorithm(vector: Vec<i8>) -> Vec<i8> {
+        let mut squared_vector: Vec<i8> = Vec::with_capacity(vector.len());
+        for num in vector.iter() {
+            squared_vector.push(*num * *num);
+        }
+        squared_vector.sort();
+        squared_vector
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<i8> = vec!(1,2,3,5,6,8,9);
+    println!("{:?}", SortedSquaredArray::sort_using_algorithm(v));
 }
